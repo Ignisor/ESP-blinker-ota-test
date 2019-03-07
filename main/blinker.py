25b155fa1d121635f31a5be3b6ac2ec4bd0f45f7
Ignisor <ignis2497@gmail.com>
2019-03-07 13:59:30 +0200

    changed structure

--- main/blinker.py
+++ main/blinker.py
@@ -7,4 +7,4 @@ LED = machine.Pin(2, machine.Pin.OUT)
 def blink():
     while True:
         LED.value(not LED.value())
-        time.sleep(1)
+        time.sleep(0.1)
---
+++ main/main.py
@@ -0,0 +1,3 @@
+from blinker import blink
+
+blink()
